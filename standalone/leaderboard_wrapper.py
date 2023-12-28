00e2c98ef94feb1280c8359c2657ee1cc6d6dafd
ignViral <127628948+ignViralX@users.noreply.github.com>
2023-12-28 11:04:05 -0500

    Code quality improvements, fix pylint

--- standalone/leaderboard_wrapper.py
+++ standalone/leaderboard_wrapper.py
@@ -25,36 +25,40 @@ if not os.path.exists("storage/"):
 # Downloads a new dataset daily to account for updates or uses todays dataset if it already exists
 path = f"storage/leaderboards_{str(date.today())}.json"
 if not os.path.exists(path):
-    data = requests.get(
-        "https://badimo.nyc3.digitaloceanspaces.com/crew_leaderboard/snapshot/top/50/season/3/latest"
-                        ".json").text
-    with open(path, mode="w") as file:
+    data = requests.get("""
+        https://badimo.nyc3.digitaloceanspaces.com/crew_leaderboard/snapshot/top/50/season/3/latest
+                        .json""",timeout=10).text
+    with open(path, mode="w", encoding="UTF-8") as file:
         file.write(str(data))
 
-file = open(path)
-loaded_file = json.load(file)
+with open(path, 'r', encoding="UTF-8") as f:
+    data = f.read()
+    f.close()
+
+loaded_data = json.loads(data)
 
 # Generates a list of every crew in the game (can be used as a Choice menu in discord bots)
 crew_list = []
 
-for obj in loaded_file:
+for obj in loaded_data:
     crew_list.append(obj["ClanName"])
 
 
 # Functions to search for crews statistics or get ordered leaderboards
 def search(crew_name: str) -> str | bytes | bytearray:
     """Gather raw statistics of a crew using its name"""
-    for loaded_object in loaded_file:
+    for loaded_object in loaded_data:
         if loaded_object["ClanName"] == crew_name:
             return loaded_object
+    return 404
 
 
 def generate_crew_data(crew_name: str) -> str:
     """Gather statistics of a crew using its name and organize them neatly into a message."""
     json_data = search(crew_name)
 
-    owner_username = requests.get(f"""https://users.roblox.com/v1/users/{str(json_data['OwnerUserId']
-                                                                           )}""").json()['name']
+    owner_username = requests.get(f"""https://users.roblox.com/v1/users/
+                                  {str(json_data['OwnerUserId'])}""",timeout=10).json()['name']
     crew_name = str(json_data["ClanName"])
     battles_played = str(json_data["BattlesPlayed"])
     battles_won = str(json_data["BattlesWon"])
@@ -77,13 +81,15 @@ def generate_crew_data(crew_name: str) -> str:
     before calling API to get a name from an ID Iterate through the 'MemberUserIds' value of the
     crew you are looking at for all of the member's IDs, check cache & then call API if the name
     isn't there. Append to the message by adding a line at the bottom **MEMBERS** »
-    {','.join(member_usernames)} where member_usernames is your list of all the names."""
+    {','.join(member_usernames)} where member_usernames is your list of all the names.
+    """
 
+    win_rate = int(battles_won) / int(battles_played) * 100
     message = f"""
         **CREW NAME** » {crew_name}
         **OWNER** » {owner_username}
         **RATING** » {rating}\n
-        **BATTLES WON (Season)** » {battles_won}/{battles_played} ({(int(battles_won) / int(battles_played)) * 100}% 
+        **BATTLES WON (Season)** » {battles_won}/{battles_played} ({win_rate}% WR)
         win rate) -> This shows the amount of battles this crew has won out of the amount they have played, 
         and their win percentage. Data limited to this season.\n
         **LATEST BATTLE (UTC)** » {last_battle_played} -> 
@@ -103,7 +109,7 @@ def fetch_latest_leaderboards(order_by: str = 'BattlesWon') -> tuple[list[Any], 
     development, and the second is a nicely formatted leaderboards message that can be used to send to players."""
 
     # Sort the list of dictionaries in descending order based on 'value'
-    sorted_data = sorted(loaded_file, key=lambda x: x[order_by], reverse=True)
+    sorted_data = sorted(loaded_data, key=lambda x: x[order_by], reverse=True)
 
     # Get the top 10 players
     top_10 = sorted_data[:10]
