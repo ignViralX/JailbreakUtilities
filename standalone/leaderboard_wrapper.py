22797f0c50fd60a77559d10a4f049d390c40d63b
ignViral <127628948+ignViralX@users.noreply.github.com>
2023-12-28 10:09:39 -0500

    Major code quality improvements

--- standalone/leaderboard_wrapper.py
+++ standalone/leaderboard_wrapper.py
@@ -4,9 +4,9 @@ MIT License
 
 Copyright (c) 2023 ignViral
 
-This is a barebones standalone wrapper for the Jailbreak Crew Leaderboard API.
+This is a bare bones standalone wrapper for the Jailbreak Crew Leaderboard API.
 This file can be copy and pasted directly into a new location and it will function fine.
-If you plan on using the entire Jailbreak Utilities toolset, just import leaderboards from the module, don't use this file.
+If you plan on using the entire Jailbreak Utilities toolset, just import leaderboards from the module.
 
 TODO: Implement members & caching system to generateCrewData()
 TODO: Minor code quality improvements
@@ -15,15 +15,17 @@ TODO: Minor code quality improvements
 
 import json, os, requests
 from datetime import date
+from typing import Tuple, List, Any
 
 # On the first run, the storage folder won't exist, so the program will make it
 if not os.path.exists("storage/"):
     os.mkdir("storage")
 
-# Downloads a new dataset daily to account for updates; if todays has already been downloaded, uses it instead of a fresh download.
+# Downloads a new dataset daily to account for updates or uses todays dataset if it already exists
 path = f"storage/leaderboards_{str(date.today())}.json"
 if not os.path.exists(path):
-    data = (requests.get("https://badimo.nyc3.digitaloceanspaces.com/crew_leaderboard/snapshot/top/50/season/3/latest.json").text)
+    data = requests.get("https://badimo.nyc3.digitaloceanspaces.com/crew_leaderboard/snapshot/top/50/season/3/latest"
+                        ".json").text
     with open(path, mode="w") as file:
         file.write(str(data))
 
@@ -36,23 +38,25 @@ crew_list = []
 for obj in loaded_file:
     crew_list.append(obj["ClanName"])
 
-# Functions to search for crews statistics or get ordered leaderboards
-def search(crewName: str) -> str | bytes | bytearray:
-    """Gather raw statistics of a crew using its name"""
-    for object in loaded_file:
-        if object["ClanName"] == crewName:
-            return(object)
-    
-def generateCrewData(crewName: str) -> str:
-    """Gather statistics of a crew using its name and organize them neatly into a message to send"""
-    data = search(crewName)
 
-    owner_username = requests.get(f"https://users.roblox.com/v1/users/{str(data['OwnerUserId'])}").json()['name']
-    crew_name = str(data["ClanName"])
-    battles_played = str(data["BattlesPlayed"])
-    battles_won = str(data["BattlesWon"])
-    rating = str(data["Rating"])
-    last_battle_played = str(data["LastBattlePlayedUTCStr"]).replace("_", " at ")
+# Functions to search for crews statistics or get ordered leaderboards
+def search(crew_name: str) -> str | bytes | bytearray:
+    """Gather raw statistics of a crew using its name"""
+    for loaded_object in loaded_file:
+        if loaded_object["ClanName"] == crew_name:
+            return loaded_object
+
+
+def generate_crew_data(crew_name: str) -> str:
+    """Gather statistics of a crew using its name and organize them neatly into a message to send"""
+    json_data = search(crew_name)
+
+    owner_username = requests.get(f"https://users.roblox.com/v1/users/{str(json_data['OwnerUserId'])}").json()['name']
+    crew_name = str(json_data["ClanName"])
+    battles_played = str(json_data["BattlesPlayed"])
+    battles_won = str(json_data["BattlesWon"])
+    rating = str(json_data["Rating"])
+    last_battle_played = str(json_data["LastBattlePlayedUTCStr"]).replace("_", " at ")
 
     """
     In this demo, "members" is not displayed, as you would need to convert all of the users IDs
@@ -64,31 +68,37 @@ def generateCrewData(crewName: str) -> str:
     even that helpful to be able to view them on other applications, just convenient. I've put some
     useful segments of code below if you do wish to implement this system.
 
-    UserID to Username API endpoint: 'https://users.roblox.com/v1/users/{username}'; returns a JSON array with a "name" value in it
-    Add another file to storage/ like names.json where you can store IDs to Names and reference it before calling API to get a name from an ID
-    Iterate through the 'MemberUserIds' value of the crew you are looking at for all of the member's IDs, check cache & then call API if the name isnt there
-    Append to the message by adding a line at the bottom **MEMBERS** » {','.join(member_usernames)} where member_usernames is your list of all the names
-    """
+    UserID to Username API endpoint: 'https://users.roblox.com/v1/users/{username}'; returns a JSON array with a 
+    "name" value in it. Add another file to storage/ like names.json where you can store IDs to Names and reference it 
+    before calling API to get a name from an ID Iterate through the 'MemberUserIds' value of the crew you are looking 
+    at for all of the member's IDs, check cache & then call API if the name isn't there. Append to the message by 
+    adding a line at the bottom **MEMBERS** » {','.join(member_usernames)} where member_usernames is your list of all 
+    the names"""
 
     message = f"""
         **CREW NAME** » {crew_name}
         **OWNER** » {owner_username}
         **RATING** » {rating}\n
-        **BATTLES WON (Season)** » {battles_won}/{battles_played} ({(int(battles_won)/int(battles_played))*100}% win rate)
-        -> This shows the amount of battles this crwe has won out of the amount they have played, and their win percentage. Data limited to this season.\n
-        **LATEST BATTLE (UTC)** » {last_battle_played}
-        -> This displays the last time, in UTC, that this crew participated in a battle. Displayed in YYYY-MM-DD_hh:mm:ss format.\n
-        """
+        **BATTLES WON (Season)** » {battles_won}/{battles_played} ({(int(battles_won) / int(battles_played)) * 100}% 
+        win rate) -> This shows the amount of battles this crew has won out of the amount they have played, 
+        and their win percentage. Data limited to this season.\n **LATEST BATTLE (UTC)** » {last_battle_played} -> 
+        This displays the last time, in UTC, that this crew participated in a battle. Displayed in 
+        YYYY-MM-DD_hh:mm:ss format.\n"""
 
     return message
 
+
 # Optional choice list to give the user when they are selecting their type of order
 choices = ['Rating', 'BattlesPlayed', 'BattlesWon']
-def fetchLatestLeaderboards(orderBy: str = 'BattlesWon') -> str | bytes | bytearray:
-    """Orders top 10 crews based on either Rating, WinPercentage, BattlesPlayed, or BattlesWon; defaults to BattlesWon. Returns two values, the first is the JSON arrays for the top 10 crews which you can use in your development, and the second is a nicely formatted leaderboards message that can be used to send to players."""
-   
+
+
+def fetch_latest_leaderboards(order_by: str = 'BattlesWon') -> tuple[list[Any], str | Any]:
+    """Orders top 10 crews based on either Rating, WinPercentage, BattlesPlayed, or BattlesWon; defaults to
+    BattlesWon. Returns two values, the first is the JSON arrays for the top 10 crews which you can use in your
+    development, and the second is a nicely formatted leaderboards message that can be used to send to players."""
+
     # Sort the list of dictionaries in descending order based on 'value'
-    sorted_data = sorted(loaded_file, key=lambda x: x[orderBy], reverse=True)
+    sorted_data = sorted(loaded_file, key=lambda x: x[order_by], reverse=True)
 
     # Get the top 10 players
     top_10 = sorted_data[:10]
@@ -96,6 +106,6 @@ def fetchLatestLeaderboards(orderBy: str = 'BattlesWon') -> str | bytes | bytear
     # Display the leaderboard
     message = """"""
     for i, crew in enumerate(top_10, start=1):
-        message = message + (f"#{i}: {crew['ClanName']} ({crew[orderBy]} {orderBy})\n")
+        message = message + f"#{i}: {crew['ClanName']} ({crew[order_by]} {order_by})\n"
 
     return top_10, message
--- standalone/trading_wrapper.py
+++ standalone/trading_wrapper.py
@@ -6,7 +6,7 @@ Copyright (c) 2023 ignViral
 
 This is a standalone wrapper for the Jailbreak Trading API.
 This file can be copy and pasted directly into a new location and it will function fine.
-If you plan on using the entire Jailbreak Utilities toolset, just import trading from the module, don't use this file.
+If you plan on using the entire Jailbreak Utilities toolset, just import trading from the module.
 
 TODO: Minor code quality improvements
 
@@ -19,10 +19,11 @@ from datetime import date
 if not os.path.exists("storage/"):
     os.mkdir("storage")
 
-# Downloads a new dataset daily to account for updates; if todays has already been downloaded, uses it instead of a fresh download.
+# Downloads a new dataset daily to account for updates; uses today's if it already exists.
 path = f"storage/trading_{str(date.today())}.json"
 if not os.path.exists(path):
-    data = (requests.get("https://badimo.nyc3.digitaloceanspaces.com/trade/frequency/snapshot/month/latest.json").text).replace("'", '"')
+    data = requests.get(
+        "https://badimo.nyc3.digitaloceanspaces.com/trade/frequency/snapshot/month/latest.json").text.replace("'", '"')
     with open(path, mode="w") as file:
         file.write(str(data))
 
@@ -35,17 +36,19 @@ item_list = []
 for obj in loaded_file:
     item_list.append(obj["name"])
 
+
 # Functions to search for items in the list and gather data; import and use these in your applications!
 def search(item: str) -> str | bytes | bytearray:
     """Gather statistics of a trading item using its name"""
-    for object in loaded_file:
-        if object["name"] == item:
-            return(object)
-    
-def generateTradingData(item: str) -> str:
+    for trading_object in loaded_file:
+        if trading_object["name"] == item:
+            return trading_object
+
+
+def generate_trading_data(item: str) -> str:
     """Gather statistics of a trading item using its name and organize them neatly into a message to send"""
     search_result = search(item)
-    type = str(search_result["type"])
+    item_type = str(search_result["type"])
 
     name = str(search_result["name"])
     wiki_name = name.replace(" ", "_")
@@ -54,7 +57,7 @@ def generateTradingData(item: str) -> str:
     demand_multiple = str(search_result["demand_multiple"])
 
     message = f"""
-        **ITEM type** » {type}
+        **ITEM type** » {item_type}
         **ITEM name** » {name}
         **ITEM WIKI** » https://jailbreak.fandom.com/wiki/{wiki_name}\n
         **TIMES TRADED (Last 30d)** » {times_traded} copies
@@ -63,7 +66,7 @@ def generateTradingData(item: str) -> str:
         -> Unique TT is the amount of unique (different copies) of this item that have changed inventories at least once this month. Be warned, this currently counts dupes as the same item, so it is not 100% accurate!\n
         **DEMAND MULTIPLE** » {demand_multiple}
         -> Demand Multiple is the average of how many times any given copy of this item that was traded this month changed inventories. Calculated using the forumla Times Traded divided by Unique Circulation.\n
-        **RARITY** » {int(unique_circulation)/1000}
+        **RARITY** » {int(unique_circulation) / 1000}
         -> Rarity is a 1-100 scale of predicted rarity of the item based off of the number of unique copies traded at least once this month (Unique TT). The higher the rarity, the more common. The lower, the rarer.\n
         """
 
